b56a328b03fd85cab21902a920ee1cbfc5faeedc
Edad Vega <107758423+EdadVega@users.noreply.github.com>
2024-03-11 10:23:28 -0500

    second commit

--- src/components/layouts/auth/Login.jsx
+++ src/components/layouts/auth/Login.jsx
@@ -1,13 +1,24 @@
-const Login = () => {
+import {useState}  from "react"
+const Login = () =>{
+    const [getUser,setUser] = useState("")
+    const [getPass, setPass] = useState("")
+    function validarInicioSesion(){
+        if(getUser == "jaime" && getPass == "123"){
+            console.log("Inicio de sesion correcta");
+        } else{
+            console.log("Incorrecto");
+        }
+    }
     return(
         <form action="">
             <section>
-                <input placeholder="Usuer" type="text" />
-                <input placeholder="Pass" type="password" />
+                <input onChange={(e)=> setUser(e.target.value)} placeholder="Usuer" type="text" />
+                <input onChange={(e)=> setPass(e.target.value)} placeholder="Pass" type="password" />
             </section>
-            <button type="button">Iniciar sesion</button>
+            <button onClick={validarInicioSesion} type="button">Iniciar sesion</button>
         </form>
     )
 }
+
 
 export default Login
